7e2678652f704216f39e266835f90b0d3135716a
DanilG73 <123488618+DanilG73@users.noreply.github.com>
2023-10-15 19:11:30 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppДанилГ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Практическая работа №2
+            //Задание 1
+            //Console.Write("a=");
+            //int a = int.Parse(Console.ReadLine());
+            //Console.Write("b=");
+            //int b = int.Parse(Console.ReadLine());
+            //int с;
+            //с = a + b;
+            //Console.WriteLine(с);
+            //Console.ReadKey();
+
+            //Задание 2
+            //Console.Write("n = ");
+            //int n = int.Parse(Console.ReadLine());
+
+            //if (n / 1000 > 0) Console.WriteLine("Invalid number");
+            //else
+            //{
+            //    int sum = 0;
+            //    while (n > 0)
+            //    {
+            //        int rem;
+            //        n = Math.DivRem(n, 10, out rem);
+            //        sum += rem;
+            //       }
+            //    Console.WriteLine("Sum = {0}", sum);
+            //    }
+            //Console.ReadKey();
+
+            //Задание 3
+            //decimal a = 2;
+            //decimal b = 2;
+            //decimal S = (a * b) / 2;
+            //Console.WriteLine(Convert.ToString(S));
+            //Console.ReadKey();
+
+            //Практическая работа 3
+            //Задание 1
+
+            //Задание 2
+            //Console.Write("m =");
+            //int m = int.Parse(Console.ReadLine());
+            //Console.Write("n =");
+            //int n = int.Parse(Console.ReadLine());
+            //m += n;
+            //n = m - n;
+            //m -= n;
+            //Console.WriteLine("{0} {1}", m, n);
+            //Console.ReadKey();
+            
+            //Задание 3
+            
+            //Задание 4
+            //double a = 2; 
+            //double a2 = a * a; 
+            //double a4 = a2 * a2; 
+            //double a8 = a4 * a4; 
+            //double a12 = a8 * a4; 
+            //double a13 = a12 * a; 
+
+            //Console.WriteLine(a13);
+            //Console.ReadKey();
+        }
+    }
+}
